226fb02c71fea75133888250b604fe24dd2a1212
PhucBruh <phucdev000@gmail.com>
2024-08-17 16:26:22 +0700

    change collapse, expaned icon

--- nvim/.config/nvim/lua/plugins/lang-tools/sql.lua
+++ nvim/.config/nvim/lua/plugins/lang-tools/sql.lua
@@ -28,15 +28,42 @@ return {
     end
 
     local map = vim.keymap.set
-    vim.g.db_ui_use_nerd_fonts = 1
     vim.g.db_ui_use_nvim_notify = 1
     vim.g.db_ui_show_help = 0
     vim.g.db_ui_debug = 1
+    vim.g.db_ui_icons = {
+      expanded = {
+        db = " 󰆼",
+        buffers = " ",
+        saved_queries = " ",
+        schemas = " ",
+        schema = " 󰙅",
+        tables = " ",
+        table = " 󰓫",
+      },
+      collapsed = {
+        db = " 󰆼",
+        buffers = " ",
+        saved_queries = " ",
+        schemas = " ",
+        schema = " 󰙅",
+        tables = " ",
+        table = " ",
+      },
+      saved_query = "",
+      new_query = "  󰓰",
+      tables = "",
+      buffers = "",
+      add_connection = "󱘖",
+      connection_ok = "✓",
+      connection_error = "✕",
+    }
 
     -- Disable some ui in DBUI
     vim.api.nvim_create_autocmd("FileType", {
       pattern = { "dbui", "dbout" },
       callback = function()
+        vim.wo.winfixwidth = true
         vim.wo.winbar = nil
         vim.wo.number = false
         vim.o.foldenable = false
