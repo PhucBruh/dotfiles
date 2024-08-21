726fe913687beaf6c2e3da703b8bf92eef74d520
PhucBruh <phucdev000@gmail.com>
2024-08-21 16:52:07 +0700

    ...

--- nvim/.config/nvim/lua/plugins/ui/which-key/setup.lua
+++ nvim/.config/nvim/lua/plugins/ui/which-key/setup.lua
@@ -2,6 +2,9 @@ local wk = require("which-key")
 
 wk.add({
   { "<leader>f", group = "Telescope", icon = "" },
+  -- { "<leader>e", icon = "󰌀" },
+  -- { "<leader>o", icon = "󰌀" },
+  { "<leader>b", group = "Buffer" },
   { "<leader>m", group = "Multicurcors", icon = "󰇀" },
   { "<leader><Tab>", group = "DBUI", icon = "󰆼" },
 
@@ -9,8 +12,10 @@ wk.add({
   { "<leader>C", icon = "✗" },
 
   { "<leader>w", desc = "Save", icon = "󰆓" },
+
+  -- { "<leader>x", group = "Trouble", icon = "" },
 })
 
 wk.setup({
-  preset = "helix",
+  preset = "modern",
 })
