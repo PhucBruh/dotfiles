b2491ce173b2420e6e9b081cbdcddc22e6af5560
PhucBruh <phucdev000@gmail.com>
2024-08-21 16:46:45 +0700

    ...

--- nvim/.config/nvim/lua/plugins/editor/comment.lua
+++ nvim/.config/nvim/lua/plugins/editor/comment.lua
@@ -6,8 +6,8 @@ return {
       local comment = require("Comment.api")
       local map = vim.keymap.set
       -- stylua: ignore
-      -- map( "n", "<leader>/", comment.call("toggle.linewise.current", "g@$"), { expr = true, desc = "Toggle comment line" })
-      -- map("v", "<leader>/", comment.call("toggle.linewise", "g@"), { expr = true, desc = "Toggle comment block" })
+      map("n", "<leader>/", comment.call("toggle.linewise.current", "g@$"), { expr = true, desc = "Toggle comment line" })
+      map("v", "<leader>/", comment.call("toggle.linewise", "g@"), { expr = true, desc = "Toggle comment block" })
       map("n", "gco", "o<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>", { desc = "Add Comment Below" })
       map("n", "gcO", "O<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>", { desc = "Add Comment Above" })
     end,
