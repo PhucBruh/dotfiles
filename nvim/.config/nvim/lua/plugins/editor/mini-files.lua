be60f1f4a32b627f64e1fee52801790df8875003
PhucBruh <phucdev000@gmail.com>
2024-08-29 15:14:31 +0700

    ...

--- nvim/.config/nvim/lua/plugins/editor/mini-files.lua
+++ nvim/.config/nvim/lua/plugins/editor/mini-files.lua
@@ -3,14 +3,14 @@ return {
   version = "*",
   keys = {
     {
-      "<C-o>",
+      "<leader>E",
       function()
         require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
       end,
       desc = "Open mini.files (Directory of Current File)",
     },
     {
-      "<C-n>",
+      "<leader>e",
       function()
         require("mini.files").open(vim.uv.cwd(), true)
       end,
